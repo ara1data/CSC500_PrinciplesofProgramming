47315ebc03104c3b9aaa2a5aded65179a8e4909c
ara1data <alex.reichart1@gmail.com>
2025-02-03 23:37:41 -0500

    Create Module_8_Final_Submission.py

---
+++ PortfolioProject/Module_8_Final_Submission.py
@@ -0,0 +1,88 @@
+# Part 1 from Milestone 1
+
+
+
+# Part 2 from Milestone 1
+
+
+
+# Part 3 from Milestone 1
+
+
+
+# Part 4 from Milestone 2
+
+
+
+# Part 5 from Milestone 2
+
+
+
+# Part 6 from Milestone 2
+
+
+
+# Part 7 Assignment Prompt
+'''
+In the main section of your code, prompt the user for a customer's name and today's date. Output the name and date. Create an object of type ShoppingCart.
+
+Example:
+Enter customer's name:
+John Doe
+Enter today's date:
+February 1, 2020
+Customer name: John Doe
+Today's date: February 1, 2020
+'''
+
+
+
+
+# Part 8 Assignment Prompt
+'''
+Implement Add item to cart menu option.
+
+Example:
+ADD ITEM TO CART
+Enter the item name:
+Nike Romaleos
+Enter the item description:
+Volt color, Weightlifting shoes
+Enter the item price:
+189
+Enter the item quantity:
+2
+'''
+
+
+
+
+
+# Part 9 Assignment Prompt
+'''
+Implement remove item menu option.
+
+Example:
+REMOVE ITEM FROM CART
+Enter name of item to remove:
+Chocolate Chips
+'''
+
+
+
+
+
+# Part 10 Assignment Prompt
+'''
+Implement Change item quantity menu option. Hint: Make new ItemToPurchase object before using ModifyItem() method.
+
+Example:
+CHANGE ITEM QUANTITY
+Enter the item name:
+Nike Romaleos
+Enter the new quantity:
+3
+'''
+
+
+
