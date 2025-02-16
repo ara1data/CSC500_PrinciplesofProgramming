1c834d2fa6bb85a695c8036e9ad51bbdd5f46967
Reichart-Anderson, Alex (NBCUniversal) <Alex.Reichart-Anderson@nbcuni.com>
2025-02-16 00:50:13 -0500

    Added module 1 componts to the module 8 file

--- PortfolioProject/Module8_FinalSubmission/Module_8_Final_Submission.py
+++ PortfolioProject/Module8_FinalSubmission/Module_8_Final_Submission.py
@@ -1,14 +1,38 @@
 # Part 1 from Milestone 1
 
+class ItemToPurchase:
+    def __init__(self, item_name="none", item_price=0.0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
 
+    def print_item_cost(self):
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${self.item_price * self.item_quantity}")
 
 # Part 2 from Milestone 1
 
+if __name__ == "__main__":
+    item1 = ItemToPurchase()
+    item2 = ItemToPurchase()
 
+    print("Item 1")
+    item1.item_name = input("Enter the item name: ")
+    item1.item_price = float(input("Enter the item price: "))
+    item1.item_quantity = int(input("Enter the item quantity: "))
+
+    print("Item 2")
+    item2.item_name = input("Enter the item name: ")
+    item2.item_price = float(input("Enter the item price: "))
+    item2.item_quantity = int(input("Enter the item quantity: "))
 
 # Part 3 from Milestone 1
 
+print("\nTOTAL COST")
+item1.print_item_cost()
+item2.print_item_cost()
 
+total_cost = (item1.item_price * item1.item_quantity) + (item2.item_price * item2.item_quantity)
+print(f"Total: ${total_cost}")
 
 # Part 4 from Milestone 2
 
